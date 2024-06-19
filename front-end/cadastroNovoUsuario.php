aefc8e1f67e2d5b0fd78bc89ee023490e8c07216
Ryan Rodrigues <ryanneno1590@gmail.com>
2024-06-18 22:20:19 -0300

    ajustes header

--- front-end/cadastroNovoUsuario.php
+++ front-end/cadastroNovoUsuario.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head> 
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Novo Usuário</title>
@@ -54,12 +55,16 @@
 
     <div class="header text-center">
         <div class="container mt-3">
-            <div class="row">
-                
-                <div class="col-4 d-flex justify-content-center align-items-center ms-auto">
+            <div class="row align-items-center">
+                <div class="col-4 d-flex justify-content-center align-items-center">
+                    <img class="mb-2" src="./img/zegotinha.jpeg" alt="logo" style="width: 70px;">
+                </div>
+                <div class="col-4 d-none d-md-block">
+                    <!-- Espaço reservado para centralização -->
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-center">
                     <div class="dropdown me-2">
-                        <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fas fa-cog"></i>
                             <span class="visually-hidden">Configurações de acessibilidade</span>
                         </button>
@@ -69,8 +74,7 @@
                         </ul>
                     </div>
                     <div class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fas fa-user me-2"></i>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
@@ -83,43 +87,41 @@
         </div>
     </div>
 
+
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
-        <div class="row"> 
-            <div class="col-md-12 register-container p-4 bg-white rounded shadow">
-                <h1 class="mb-4">Cadastro Usuário</h1>
-                <form id="register-form">
-                    <div class="form-group position-relative">
-                        <label for="fullname">Nome Completo:</label>
-                        <input type="text" id="fullname" name="fullname" class="form-control" required autofocus>
-                    </div>
-                    <div class="form-group position-relative">
-                        <label for="email">E-mail:</label>
-                        <input type="email" id="email" name="email" class="form-control" required>
-                    </div>
-                    <div class="form-group position-relative">
-                        <label for="username">Nome de Usuário:</label>
-                        <input type="text" id="username" name="username" class="form-control" required>
-                    </div>
-                    <div class="form-group position-relative">
-                        <label for="password">Senha:</label>
-                        <input type="password" id="password" name="password" class="form-control" required>
-                        <span class="toggle-password" onclick="togglePasswordVisibility('password', this)">
-                            <i class="fa fa-eye"></i>
-                        </span>
-                    </div>
-                    <div class="form-group position-relative">
-                        <label for="confirm-password">Confirmação de Senha:</label>
-                        <input type="password" id="confirm-password" name="confirm-password" class="form-control"
-                            required>
-                        <span class="toggle-password" onclick="togglePasswordVisibility('confirm-password', this)">
-                            <i class="fa fa-eye"></i>
-                        </span>
-                    </div>
-                    <button type="submit" id="register-btn" class="btn btn-primary btn-block">Cadastrar</button>
-                </form>
-                <div id="success-message" class="alert alert-success mt-4 d-none" role="alert">
-                    Cadastro concluído!
+        <div class="register-container p-4 bg-white rounded shadow">
+            <h1 class="mb-4">Cadastro de Novo Usuário</h1>
+            <form id="register-form">
+                <div class="form-group position-relative">
+                    <label for="fullname">Nome Completo:</label>
+                    <input type="text" id="fullname" name="fullname" class="form-control" required autofocus>
                 </div>
+                <div class="form-group position-relative">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+                <div class="form-group position-relative">
+                    <label for="username">Nome de Usuário:</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+                <div class="form-group position-relative">
+                    <label for="password">Senha:</label>
+                    <input type="password" id="password" name="password" class="form-control" required>
+                    <span class="toggle-password" onclick="togglePasswordVisibility('password', this)">
+                        <i class="fa fa-eye"></i>
+                    </span>
+                </div>
+                <div class="form-group position-relative">
+                    <label for="confirm-password">Confirmação de Senha:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" class="form-control" required>
+                    <span class="toggle-password" onclick="togglePasswordVisibility('confirm-password', this)">
+                        <i class="fa fa-eye"></i>
+                    </span>
+                </div>
+                <button type="submit" id="register-btn" class="btn btn-primary btn-block">Cadastrar</button>
+            </form>
+            <div id="success-message" class="alert alert-success mt-4 d-none" role="alert">
+                Cadastro concluído!
             </div>
         </div>
     </div>
@@ -134,7 +136,7 @@
             iconClass.classList.toggle('fa-eye-slash');
         }
 
-        document.getElementById('register-form').addEventListener('submit', function (event) {
+        document.getElementById('register-form').addEventListener('submit', function(event) {
             event.preventDefault();
             var successMessage = document.getElementById('success-message');
             var password = document.getElementById('password').value;
@@ -142,7 +144,7 @@
 
             var formFields = document.querySelectorAll('#register-form input');
             var formValid = true;
-            formFields.forEach(function (field) {
+            formFields.forEach(function(field) {
                 if (!field.value.trim()) {
                     field.classList.add('is-invalid');
                     formValid = false;
@@ -174,16 +176,16 @@
             var form = event.target;
             form.reset();
 
-            setTimeout(function () {
+            setTimeout(function() {
                 successMessage.classList.add('d-none');
             }, 3000);
         });
     </script>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
 
 </body>
